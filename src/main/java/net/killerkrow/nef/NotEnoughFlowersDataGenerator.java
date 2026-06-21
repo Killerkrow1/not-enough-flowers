dc1699015c330e2842e14139a29d81abde94dfcb
Killerkrow <cpf283@gmail.com>
2026-06-21 14:45:00 -0500

    Made every flower have a dye recipe

--- src/main/java/net/killerkrow/nef/NotEnoughFlowersDataGenerator.java
+++ src/main/java/net/killerkrow/nef/NotEnoughFlowersDataGenerator.java
@@ -2,10 +2,14 @@ package net.killerkrow.nef;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.killerkrow.nef.util.ModRecipeProvider;
 
 public class NotEnoughFlowersDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModRecipeProvider::new);
 
 	}
 }
